3125b98c7c113f926db15311ad267e600d5c156e
Dari028 <dario.pantoja@udea.edu.co>
2024-03-03 15:58:23 -0500

    acabar la funcio de ver los datos del paciente

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -27,32 +27,25 @@ class Sistema:
     def __init__(self):
         self.__dict_pacientes_ced = {}
         self.__dict_pacientes_nom = {}
-        self.__NumeroPacientes=len(self.__dict_pacientes_nom)
-         
-    def buscar_paciente_ced(self, c):
-        
-        if c in self.__dict_pacientes_ced:
-            return True
-        else:  
-            return False
-    def buscar_paciente_nom(self, c):
-        
-        if c in self.__dict_pacientes_nom:
-            return True
-        else:  
-            return False
-
     
+   
+    def buscar_paciente_ced(self, clave):
+        encontrado = False
+        if clave in self.__dict_pacientes_ced:
+            encontrado = True
+        return encontrado
 
+    def buscar_paciente_nom(self, clave):
+        encontrado = False
+        if clave in self.__dict_pacientes_nom:
+            encontrado = True
+        return encontrado
 
     def ingresarPaciente(self,pac):
-       
         id_ = pac.verCedula()
         name_ = pac.verNombre()
 
-        if id_ in self.__dict_pacientes_ced:
-            return False
-        elif name_ in self.__dict_pacientes_nom:
+        if id_ in self.__dict_pacientes_ced or name_ in self.__dict_pacientes_nom:
             return False
             
         self.__dict_pacientes_ced[id_] = pac
@@ -60,84 +53,68 @@ class Sistema:
         return True
 
 
-    
     def verDatosPaciente(self, c):
-        if c in self.__dict_pacientes_ced:
-            return self.__dict_pacientes_ced[c]
-        elif c in self.__dict_pacientes_nom:
+        if c  in self.__dict_pacientes_ced:
+            return  self.__dict_pacientes_ced[c]
+        
+        if c  in self.__dict_pacientes_nom:
             return self.__dict_pacientes_nom[c]
         else:
             return None
 
-
     def verNumeroPacientes(self):
-        # print("Enel sistema hay: " + str(len(self.__lista_pacientes)) + " pacientes")
-        return self.__NumeroPacientes
-    def imprimir_diccionarios(self):
-        print("Diccionario de pacientes por cedula:")
-        print(self.__dict_pacientes_ced)
-        print("Diccionario de pacientes por nombre:")
-        print(self.__dict_pacientes_nom)
-sis = Sistema()
-sis1 = Sistema()
-sis2 = Sistema()
+        return len(self.__dict_pacientes_ced)
+        
 def main():
-    
+    sis = Sistema()
     while True:
-        print(sis2.imprimir_diccionarios())
-        opcion = int(input("Ingrese 0 para volver al menu, 1 para ingresar nuevo paciente, 2 ver paciente: , 3 - ver cantidad de pacientes "))
+        opcion = int(input("Ingrese 0 para volver al menu, 1 para ingresar nuevo paciente, 2 ver paciente: , 3 - ver cantidad de pacientes, 4 - salir "))
         if opcion == 1:
-            print("A continuacion se solicitaran los seguientes datos:")
-            # 1 Se solicitaran los datos
+            print("A continuacion se solicitaran los siguientes datos:")
             nombre = input("Ingrese el nombre: ")
             cedula = int(input("Ingrese la cedula: "))    
             genero = input("Ingrese el genero: ")
             servicio = input("Ingrese el servicio: ")
-            # 2 se crea un objeto Paciente
             pac = Paciente()
-            # como es paciente esta vacio debo ingresarle la informacion
             pac.asignarNombre(nombre)
             pac.asignarCedula(cedula)
             pac.asignarGenero(genero)
             pac.asignarServicio(servicio)
-            r = sis2.ingresarPaciente(pac)
-            # 3 se almacena en el diccionario que esta dentro de la clase sistema
+            r = sis.ingresarPaciente(pac)
 
             if r == True:
-                print("paciente ingresado")
-                print(sis2.imprimir_diccionarios())
+                print("Paciente ingresado")
             else:
-                print("paciente ya existe en el sistema")
+                print("Paciente ya existe en el sistema")
 
         elif opcion == 2:
-        
             cn = input("Ingrese la cedula o el nombre del paciente a buscar: ")
-            if sis2.buscar_paciente_ced(cn)==True:
-                p = sis2.verDatosPaciente(cn)
+            try:
+                cn = int(cn)
+            except ValueError:
+                pass
+
+            if sis.buscar_paciente_ced(cn):
+                p = sis.verDatosPaciente(cn)
                 print("Nombre: " + p.verNombre())
                 print("Cedula: " + str(p.verCedula()))
                 print("Genero: " + p.verGenero())
                 print("Servicio: " + p.verServicio())
-            elif sis2.buscar_paciente_nom(cn)== True:
-                p = sis2.verDatosPaciente(cn)
+            elif sis.buscar_paciente_nom(cn):
+                p = sis.verDatosPaciente(cn)
                 print("Nombre: " + p.verNombre())
                 print("Cedula: " + str(p.verCedula()))
                 print("Genero: " + p.verGenero())
                 print("Servicio: " + p.verServicio())
             else:
                 print("El paciente no se encontró")
-                print(cn)
-                print(sis.buscar_paciente_nom(cn))
-                print(sis.buscar_paciente_ced(cn))
         
         elif opcion == 3:
-            print(f"la cantidad de pacientes en el sistema es: {sis.verNumeroPacientes()}")
+            print(f"La cantidad de pacientes en el sistema es: {sis.verNumeroPacientes()}")
                     
-        elif opcion != 0:
-            continue
-        else:
+        
+        if opcion==4 :
             break
 
 if __name__ == '__main__':
     main()
-    
